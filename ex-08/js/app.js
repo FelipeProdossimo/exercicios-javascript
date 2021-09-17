a319114be43e9028e4a089a7f105cfaffceb0977
FELIPE PRODOSSIMO <felipe.prodossimo@senacsp.edu.br>
2021-09-16 22:23:05 -0300

    Novas atividades acrescentadas.

---
+++ ex-08/js/app.js
@@ -0,0 +1,8 @@
+let diaSemana = 'Sabado'
+
+if(diaSemana != 'Sabado' && diaSemana != 'Domingo'){
+    console.log("Você pode ir ao banco.")
+}
+else {
+    console.log("O banco está fechado, tente outro dia.")
+}
